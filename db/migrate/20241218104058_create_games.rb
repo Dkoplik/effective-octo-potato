a1a0479ea81a5877a8a2bdd78915fbf01d0f5bdd
Ruslan Bichurin <115139612+Ruslan2806@users.noreply.github.com>
2024-12-18 15:04:04 +0300

    исправил ошибку в миграциях

--- db/migrate/20241218104058_create_games.rb
+++ db/migrate/20241218104058_create_games.rb
@@ -1,7 +1,6 @@
 class CreateGames < ActiveRecord::Migration[8.0]
   def change
     create_table :games do |t|
-      t.integer :id
       t.integer :player1_id
       t.integer :player2_id
       t.integer :winner_id
