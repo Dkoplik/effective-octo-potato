470a5cc4983b4c20167c4af48233d13d6bdb510b
Ruslan Bichurin <115139612+Ruslan2806@users.noreply.github.com>
2024-12-18 14:43:20 +0300

    добавил модели и миграции для Game и User

---
+++ db/migrate/20241218104058_create_games.rb
@@ -0,0 +1,13 @@
+class CreateGames < ActiveRecord::Migration[8.0]
+  def change
+    create_table :games do |t|
+      t.integer :id
+      t.integer :player1_id
+      t.integer :player2_id
+      t.integer :winner_id
+      t.timestamp :started_at
+      t.timestamp :ended_at
+      t.timestamp :created_at
+    end
+  end
+end
