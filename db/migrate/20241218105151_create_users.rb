2aa557d43ef8f86e9c6890bf8895be948318dab2
Ruslan Bichurin <115139612+Ruslan2806@users.noreply.github.com>
2024-12-20 18:48:36 +0300

    изменил немного миграцию для User

--- db/migrate/20241218105151_create_users.rb
+++ db/migrate/20241218105151_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
-      t.string :password
+      t.string :username, null: false, unique: true
+      t.string :email, null: false, unique: true
+      t.string :password, null: false
 
       t.timestamps
     end
